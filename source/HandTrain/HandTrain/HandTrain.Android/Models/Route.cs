1d89b7f3fa7f1241c65707d3e790fe052c8b98b5
Sergey Pastushenko <s.pastushenko@itransition.com>
2017-12-18 17:11:20 +0300

    added model

---
+++ source/HandTrain/HandTrain/HandTrain.Android/Models/Route.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HandTrain.Droid.Models
+{
+    class Route
+    {
+        public string FirstCity { get; set; }
+        public string SecondCity { get; set; }
+        public DateTime DataTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public DateTime DepartureЕime{ get; set; }
+        public string TimeToWay { get; set; }
+        public string Price { get; set; }
+        public string AvalibleTickets { get; set; }
+    }
+}
---
+++ source/HandTrain/HandTrain/HandTrain.Android/Service/RwApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HandTrain.Droid.Service
+{
+    class RwApi
+    {
+        private string templateStringRequest = "http://rasp.rw.by/ru/route/?from={0}&from_exp=0&from_esr=0&to={1}&to_exp=0&to_esr=0&date={2}";
+    }
+}
